5a7d99760d374626653a6f6b95ed8c2c0526ef38
IkoDieko <diego_estrada14@hotmail.com>
2019-02-20 17:54:59 -0600

    alta lista

--- DB/alta_usuario.sql
+++ DB/alta_usuario.sql
@@ -1,19 +1,17 @@
-drop procedure if exists `alta_usuario`;
 DELIMITER $$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `alta_usuario`(
-    in _usuario varchar(45),
+	in _usuario varchar(45),
     in _correo varchar (45),
-    in _contraseña varchar (45),
-    in _imagen longblob
-)
+    in _pass varchar (45)
+    )
 BEGIN
-	DECLARE validar INT;
+DECLARE validar INT;
 	DECLARE ID INT;
     SELECT COUNT(*) INTO validar FROM usuario WHERE correo=_correo;
 	if validar = 0 THEN
 	SELECT (max(idUsuario)+1) INTO ID FROM usuario limit 1;
 	#the value estados_id its a auto_increment value, just call to add the value...
-	INSERT INTO usuario(idUsuario,usuario, correo, contraseña, imagen) VALUES(ID,_usuario,_correo,_contraseña, _imagen);
+	INSERT INTO usuario(idUsuario,usuario, pass, correo) VALUES(ID,_usuario,_pass,_correo);
 	CALL alta_usuarios(); #SP that show all data...
 	ELSE
 		SELECT CONCAT('El correo: ',_correo,' ya está en uso.') ESTADO;
---
+++ DB/rw_database.sql
@@ -0,0 +1,109 @@
+drop database if exists rw_database;
+create database rw_database;
+use rw_database;
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuario` (
+  `idUsuario` int(11) NOT NULL,
+  `usuario` varchar(45) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  `correo` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idUsuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+DROP TABLE IF EXISTS `tipoobra`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tipoobra` (
+  `idtipoObra` int(11) NOT NULL,
+  `tipoObra` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idtipoObra`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+LOCK TABLES `tipoobra` WRITE;
+/*!40000 ALTER TABLE `tipoobra` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tipoobra` ENABLE KEYS */;
+UNLOCK TABLES;
+
+DROP TABLE IF EXISTS `obra`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `obra` (
+  `idObra` int(11) NOT NULL,
+  `tipo` int(11) DEFAULT NULL,
+  `titulo` varchar(45) DEFAULT NULL,
+  `fecha` date DEFAULT NULL,
+  PRIMARY KEY (`idObra`),
+  KEY `tipo_idx` (`tipo`),
+  CONSTRAINT `tipo` FOREIGN KEY (`tipo`) REFERENCES `tipoobra` (`idtipoObra`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+LOCK TABLES `obra` WRITE;
+/*!40000 ALTER TABLE `obra` DISABLE KEYS */;
+/*!40000 ALTER TABLE `obra` ENABLE KEYS */;
+UNLOCK TABLES;
+
+DROP TABLE IF EXISTS `relobrausu`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `relobrausu` (
+  `idrelObraUsu` int(11) NOT NULL,
+  `idObra` int(11) DEFAULT NULL,
+  `idUsuario` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idrelObraUsu`),
+  KEY `idUsuario_idx` (`idUsuario`),
+  KEY `idObra_idx` (`idObra`),
+  CONSTRAINT `idObra` FOREIGN KEY (`idObra`) REFERENCES `obra` (`idObra`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `idUsuario` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+LOCK TABLES `relobrausu` WRITE;
+/*!40000 ALTER TABLE `relobrausu` DISABLE KEYS */;
+/*!40000 ALTER TABLE `relobrausu` ENABLE KEYS */;
+UNLOCK TABLES;
+
+DROP TABLE IF EXISTS `resumen`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `resumen` (
+  `idResumen` int(11) NOT NULL,
+  `contenido` mediumtext,
+  `idObra1` int(11) DEFAULT NULL,
+  `claves` longtext,
+  PRIMARY KEY (`idResumen`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+LOCK TABLES `resumen` WRITE;
+/*!40000 ALTER TABLE `resumen` DISABLE KEYS */;
+/*!40000 ALTER TABLE `resumen` ENABLE KEYS */;
+UNLOCK TABLES;
+
+CREATE TABLE `ensayo` (
+  `idensayo` int(11) NOT NULL,
+  `idObra2` int(11) DEFAULT NULL,
+  `intro` mediumtext,
+  `desarrollo` mediumtext,
+  `conclusion` mediumtext,
+  PRIMARY KEY (`idensayo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+LOCK TABLES `ensayo` WRITE;
+/*!40000 ALTER TABLE `ensayo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ensayo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+CREATE TABLE `referencias` (
+  `idreferencias` int(11) NOT NULL,
+  `autor` varchar(45) DEFAULT NULL,
+  `idObra3` int(11) DEFAULT NULL,
+  `año` int(11) DEFAULT NULL,
+  `fechaConsul` date DEFAULT NULL,
+  `datos` varchar(400) DEFAULT NULL,
+  PRIMARY KEY (`idreferencias`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+LOCK TABLES `referencias` WRITE;
+/*!40000 ALTER TABLE `referencias` DISABLE KEYS */;
+/*!40000 ALTER TABLE `referencias` ENABLE KEYS */;
+UNLOCK TABLES;
